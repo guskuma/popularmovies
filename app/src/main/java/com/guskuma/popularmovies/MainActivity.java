f20fa1ab2c1211026290809b0d910296a65259fa
guskuma <julioguskuma@gmail.com>
2017-02-06 08:58:31 -0200

    Movies fetching; RecyclerView

--- app/src/main/java/com/guskuma/popularmovies/MainActivity.java
+++ app/src/main/java/com/guskuma/popularmovies/MainActivity.java
@@ -2,12 +2,113 @@ package com.guskuma.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.guskuma.tmdbapi.MovieFilterDescriptor;
+import com.guskuma.tmdbapi.MovieResultSet;
+import com.guskuma.tmdbapi.TMDbClient;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements Callback<MovieResultSet>, TMDbAdapter.MovieItemClickListener {
+
+    public static final String BASE_URL = "http://api.themoviedb.org/";
+    String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.rv_movies_list) RecyclerView mMoviesList;
+    @BindView(R.id.pb_fetching)    ProgressBar mFetchProgress;
+    @BindView(R.id.tv_empty_list)  TextView mEmptyMessage;
+
+    @BindString(R.string.fetch_fail) String toastMessage;
+
+    int mLastPageLoaded = 0;
+    TMDbClient mMovieService;
+    TMDbAdapter mTMDbAdapter;
+    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Gson gson = new GsonBuilder().setLenient().create();
+        mMovieService = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(TMDbClient.class);
+
+        fetchMoviesList(1);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL, false);
+        mMoviesList.setLayoutManager(layoutManager);
+        mMoviesList.setHasFixedSize(false);
+        mMoviesList.setAdapter(mTMDbAdapter);
+
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
+    }
+
+    public void fetchMoviesList(int pageToLoad){
+        if(mTMDbAdapter == null){
+            mTMDbAdapter = new TMDbAdapter(this);
+        }
+
+        Call<MovieResultSet> call = mMovieService.getMoviesList(MovieFilterDescriptor.POPULAR, "e8a6c51fc482352ed4caed9cb105552f", "en-US", pageToLoad);
+        call.enqueue(this);
+        mFetchProgress.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onResponse(Call<MovieResultSet> call, Response<MovieResultSet> response) {
+        if(response.isSuccessful()) {
+            MovieResultSet movieResultSet = response.body();
+            mLastPageLoaded = movieResultSet.page;
+            mTMDbAdapter.addMovies(mLastPageLoaded, movieResultSet.results);
+
+            toggleViewsVisibility(true);
+        } else {
+            toggleViewsVisibility(false);
+        }
+
+        mFetchProgress.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void toggleViewsVisibility(boolean success) {
+        mEmptyMessage.setVisibility(success ? View.INVISIBLE : View.VISIBLE);
+        mMoviesList.setVisibility(success ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    @Override
+    public void onFailure(Call<MovieResultSet> call, Throwable t) {
+        if(mToast!=null){
+            mToast.cancel();
+        }
+
+        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+        Log.d(TAG, "Movie list fetch failed: " + t.getMessage());
+        mToast.show();
+        mFetchProgress.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/guskuma/popularmovies/TMDbAdapter.java
+++ app/src/main/java/com/guskuma/popularmovies/TMDbAdapter.java
@@ -1,15 +1,22 @@
 package com.guskuma.popularmovies;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.guskuma.tmdbapi.Movie;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.zip.Inflater;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Julio on 28/01/2017.
@@ -17,25 +24,45 @@ import java.util.zip.Inflater;
 
 public class TMDbAdapter extends RecyclerView.Adapter<TMDbAdapter.MovieItemViewHolder> {
 
-    ArrayList<String> mMovies = new ArrayList<>();
+    String TAG = TMDbAdapter.class.getSimpleName();
 
-    public TMDbAdapter(String[] movies){
-        mMovies.addAll(Arrays.asList(movies));
+    private int mCurrentPage;
+    private MovieItemClickListener mClickListener;
+    ArrayList<Movie> mMovies = new ArrayList<>();
+
+    public TMDbAdapter(MovieItemClickListener listener){
+        mClickListener = listener;
     }
 
-    public void addMovies(String[] moviesToAdd){
-        mMovies.addAll(Arrays.asList(moviesToAdd));
+    public void addMovies(int pageNumber, List<Movie> moviesToAdd){
+        if(pageNumber > mCurrentPage) {
+            mCurrentPage = pageNumber;
+            mMovies.addAll(moviesToAdd);
+            Log.d(TAG, String.format("%s movies addes (page %s)", moviesToAdd.size(), pageNumber));
+        }
     }
 
-    public class MovieItemViewHolder extends RecyclerView.ViewHolder {
+    public class MovieItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView mMoviePosterImageView;
         TextView mMovieTitleTextView;
 
         public MovieItemViewHolder(View itemView) {
             super(itemView);
-            mMoviePosterImageView = (ImageView) itemView.findViewById(R.id.iv_movie_poster);
-            mMovieTitleTextView = (TextView) itemView.findViewById(R.id.tv_movie_title);
+            mMovieTitleTextView = (TextView)itemView.findViewById(R.id.tv_movie_title) ;
+            mMoviePosterImageView =  (ImageView) itemView.findViewById(R.id.iv_movie_poster) ;
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mClickListener.onListItemClick(clickedPosition);
+        }
+
+    }
+
+    public interface MovieItemClickListener {
+        void onListItemClick(int clickedItemIndex);
     }
 
     @Override
@@ -46,7 +73,6 @@ public class TMDbAdapter extends RecyclerView.Adapter<TMDbAdapter.MovieItemViewH
     @Override
     public MovieItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-
         View view = inflater.inflate(R.layout.grid_item_movie, parent, false);
         MovieItemViewHolder viewHolder = new MovieItemViewHolder(view);
 
@@ -55,6 +81,15 @@ public class TMDbAdapter extends RecyclerView.Adapter<TMDbAdapter.MovieItemViewH
 
     @Override
     public void onBindViewHolder(MovieItemViewHolder holder, int position) {
+        Movie movie = mMovies.get(position);
 
+        Uri imageUri = new Uri.Builder().scheme("http").path("image.tmdb.org/t/p/").appendPath("w154").appendPath(movie.poster_path).build();
+
+        holder.mMovieTitleTextView.setText(movie.title);
+        Picasso.with(holder.itemView.getContext()).load(imageUri.toString()).into(holder.mMoviePosterImageView);
+    }
+
+    public int getmCurrentPage() {
+        return mCurrentPage;
     }
 }
